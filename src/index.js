98c39c9ea0fd9b50b5c3429ae7415a80692a68e4
GarikMSK <igor@nbma.ru>
2023-08-05 00:25:26 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 module.exports = function toReadable (number) {
- 
+
 let simpleNumbers = ["zero", "one","two","three","four","five","six","seven","eight","nine"]
-let tensNumbers = ["ten","eleven","twelve","thirteen","fourteen","fiveteen","sixteen","seventeen","eighteen","nineteen"]
-let deciNumbers = ["","","twenty", "thirdy", "fourthy", "fifty", "sixty", "seventy", "eighty", "ninegthy"]
+let tensNumbers = ["ten","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"]
+let deciNumbers = ["","","twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
 numbersArray =Array.from(String(number)).reverse()
 
 let finalText = ""
-let justNumbers = simpleNumbers[numbersArray[0]]
+if (numbersArray[0]== 0 && numbersArray[1]== 0) {justNumbers = ""; hundredWord = " hundred"}
+else {justNumbers = simpleNumbers[numbersArray[0]]; hundredWord = " hundred "}
 if(numbersArray[1] == 0 || !numbersArray[1]) {finalText = justNumbers}
 else if (numbersArray[1]== 1) {finalText = finalText + tensNumbers[numbersArray[0]]}
 else if (numbersArray[1] >=2) {
     finalText = deciNumbers[numbersArray[1]]
     if(numbersArray[0] > 0) finalText = finalText + " " + justNumbers;
-    if(numbersArray[2]) finalText = simpleNumbers[numbersArray[2]] + " hundred " + finalText
+    
 }
+if(numbersArray[2]) finalText = simpleNumbers[numbersArray[2]] + hundredWord +finalText
+console.log(finalText)
+
 return finalText
-}
+
+
+   
+ }
